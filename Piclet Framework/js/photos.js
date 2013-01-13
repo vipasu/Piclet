5aa297e85bc791a5b10a01ff6272df0949c71dcb
Vincent Su <phazedphantom@gmail.com>
2013-01-13 13:59:33 -0800

    Photo Tags part two

--- Piclet Framework/js/photos.js
+++ Piclet Framework/js/photos.js
@@ -36,12 +36,11 @@ function loadPhotos(id, isLast) {
 	}
   	//FB.api({method: 'fql.query', query: 'SELECT object_id,src_big,like_info,comment_info FROM photo WHERE object_id IN (SELECT object_id FROM photo_tag WHERE subject = ' + id + ') OR owner = '+ id}, function(response) {
     FB.api('/fql', {q:{"query1" :  
-    		"SELECT object_id,src_big,like_info,comment_info FROM photo WHERE (object_id IN (SELECT object_id FROM photo_tag WHERE subject = " + id + ")) OR (owner = " + id + ") AND like_info.like_count > 1 LIMIT 3000",
+    		"SELECT object_id,src_big,like_info,comment_info FROM photo WHERE (object_id IN (SELECT object_id FROM photo_tag WHERE subject = " + id + ")) OR (owner = " + id + ") AND like_info.like_count > 0 LIMIT 3000",
     	"query2" :
     		"SELECT object_id,text FROM photo_tag WHERE object_id IN (SELECT object_id FROM #query1)"}}, 
     function(response){
     	if ("data" in response){
-<<<<<<< HEAD
 		photos = response.data[0].fql_result_set;
 		//comments = response.data[1].fql_result_set;
         tags = response.data[1].fql_result_set;
@@ -67,53 +66,22 @@ function loadPhotos(id, isLast) {
     		var commentOb = photoArray.data[comments[i].object_id].comments;
     		commentOb[commentOb.length] = comments[i].text;
     	}*/
-    	
+
     	photoArray.length          = photos.length;
     	//photoArray.comments_length = comments.length;
     	photoArray.userId          = id;
-    	
+
 		enqueueArray(photoArray, pq);
 		photoData.users[id] = photoArray;
     	console.log("Loaded " + photoArray.length + " Photos For " + getFriendName(photoArray.userId) + ".");
-    	
-=======
-		
-			photos = response.data[0].fql_result_set;
-			//comments = response.data[1].fql_result_set;
-			photoArray = {};
-			photoArray.data = {};
-			for (var i = 0; i < photos.length; i++) {
-				photoObject = photos[i];
-				photoObject.comments = [];
-				photoObject.comment_count = photoObject.comment_info.comment_count;
-				photoObject.like_count = photoObject.like_info.like_count;
-				delete photoObject.comment_info;
-				delete photoObject.like_info;
-				photoArray.data[photoObject.object_id] = photoObject;
-			}
-			/*
-			 for (var i = 0; i < comments.length; i++){
-			 var commentOb = photoArray.data[comments[i].object_id].comments;
-			 commentOb[commentOb.length] = comments[i].text;
-			 }*/
-
-			photoArray.length = photos.length;
-			//photoArray.comments_length = comments.length;
-			photoArray.userId = id;
-
-			enqueueArray(photoArray, pq);
-			photoData.users[id] = photoArray;
-			console.log("Loaded " + photoArray.length + " Photos For " + getFriendName(photoArray.userId) + ".");
-
->>>>>>> 1a9b698d6b3634f0f56851bbebc2c3128e0900eb
     	}
-    	
+
     	__countdown--;
-    	
+
     	if (__countdown == 0){
     		display();
     	}
-    	
+
 	});
 };
 
