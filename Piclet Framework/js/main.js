6aa6579ba37940106415b490d19383d7f2a0c484
psrthegreat <pranav.rajpurkar@gmail.com>
2013-01-13 03:03:57 -0800

    CSS changes

--- Piclet Framework/js/main.js
+++ Piclet Framework/js/main.js
@@ -9,7 +9,7 @@ function displayUserNavBar(){
 	if(indexInAllFriends != -1 && indexInSearchedFriends == -1){
 		var id = getFriendId(name);
 		var url = profilePicURL(id);
-		var lihtml = '<li onclick= "callRemoveNavBar(this.id)"  id="' + id + '" class="media"><a class="pull-left" href="#"><img class="media-object" data-src="holder.js/64x64" src="' + url + '" alt="64x64"></a><div class="media-body"><h4 class="media-heading">' + name + '</h4></div></li>';
+		var lihtml = '<li onclick= "callRemoveNavBar(this.id)"  id="' + id + '" class="media" style= "cursor: pointer;"><a class="pull-left" href="#"><img class="media-object" data-src="holder.js/64x64" src="' + url + '" alt="64x64"></a><div class="media-body"><h4 class="media-heading">' + name + '</h4></div></li>';
 		//console.log(lihtml);
 		$('#list-prof').append(lihtml);
 		friendsToSearch.push(name);
