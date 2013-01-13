33f1f849e01ae1bfeb012d62e9fccf521448a95c
Charles Guan <charlesguan94@gmail.com>
2013-01-12 22:01:52 -0800

    Adapts previous file to photoObject in FQL format instead of Graph API

--- Piclet Framework/js/importance.js
+++ Piclet Framework/js/importance.js
@@ -1,4 +1,49 @@
 function getLikeCount(photoObject){
+    /*var count = 0;
+    for (like in photoObject.likes.data){
+        count++;
+    }*/
+    return photoObject.like_count;
+}
+
+function getCommentCount(photoObject){
+    /*var count = 0;
+    for (comment in photoObject.comments.data){
+        count++;
+    }
+    return count;*/
+   return photoObject.comment_count;
+}
+
+function ParseComment(comment){
+    var bonus = 0;
+    var positive = ["GOOD", "GREAT", "BEAUTIFUL", "HANDSOME", "SWEET", "PRETTY", ":)"]
+    //var negative = ["UGLY", "BAD", "SAD", "TERRIBLE"]
+    for (phrase in positive){
+        if (comment.text.toUpperCase().find(phrase)!=-1)
+            bonus += 1;
+    }
+    /*for (phrase in negative){
+        if (message.find(phrase)!=-1){
+            bonus -= 1;
+        }
+    }*/
+    bonus *= (1 + Math.sqrt(comment.likes))
+    return bonus;
+}
+
+
+function importanceFactor(photoObject){
+    var weight = 0;
+    weight += getLikeCount(photoObject) + getCommentCount(photoObject);
+    for (comment in photoObject.comments){
+        weight += ParseComment(comment);
+    }
+    return weight;
+}
+
+/*
+function getLikeCount(photoObject){
     var count = 0;
     for (like in photoObject.likes.data){
         count++;
@@ -22,11 +67,11 @@ function ParseComment(comment){
         if (comment.message.toUpperCase().find(phrase)!=-1)
             bonus += 1;
     }
-    /*for (phrase in negative){
+    for (phrase in negative){
         if (message.find(phrase)!=-1){
             bonus -= 1;
         }
-    }*/
+    }
     bonus *= (1 + Math.sqrt(comment.like_count))
     return bonus;
 }
@@ -40,3 +85,5 @@ function importanceFactor(photoObject){
     }
     return weight;
 }
+
+*/
